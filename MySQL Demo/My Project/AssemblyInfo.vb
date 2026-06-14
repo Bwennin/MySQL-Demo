40a15a891e1ca8698c8fe494f8c5b252b2dee21f
Bwennin <brandon.miles98@yahoo.com>
2026-06-14 17:36:29 -0500

    Add project files.

---
+++ MySQL Demo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MySQL Demo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("MySQL Demo")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e23de829-2806-40fc-aa76-182543d9f724")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ MySQL Demo/Program.vb
@@ -0,0 +1,47 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Program
+
+    Sub Main()
+        Dim strUser As String
+        Dim strPassword As String
+        Dim strConnection As String
+
+        Console.Write("User: ")
+        strUser = Console.ReadLine()
+        Console.Write("Password: ")
+        strPassword = Console.ReadLine()
+
+        strConnection = $"server=localhost;user={strUser};database=sakila;port=3306;password={strPassword}"
+
+        Using oConnection As New MySqlConnection(strConnection)
+            Try
+                Console.WriteLine("Connecting to MySQL..." & vbNewLine)
+                oConnection.Open()
+
+                Dim strSQL As String = "SELECT * FROM sakila.actor LIMIT 0,10;"
+                Dim cmd As New MySqlCommand(strSQL, oConnection)
+                Dim rdr As MySqlDataReader = cmd.ExecuteReader()
+
+                While rdr.Read()
+                    Console.WriteLine($"{rdr(0):00} - {rdr(1)} {rdr(2)}")
+                End While
+
+                Console.WriteLine()
+            Catch ex As InvalidOperationException
+                Console.WriteLine("Unable to load data. Make sure your database connection is running.")
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString())
+
+                If ex.InnerException IsNot Nothing Then
+                    Console.WriteLine(ex.InnerException.ToString())
+                End If
+            Finally
+                oConnection.Close()
+                Console.WriteLine("Connection closed. Press any key to exit...")
+                Console.ReadLine()
+            End Try
+        End Using
+    End Sub
+
+End Module
